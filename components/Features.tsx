f2e5b6a1ae823c34a2b41e0e66699c25d2d0e9c3
Gladiale <the_Garden_of_sinners@protonmail.com>
2024-09-29 19:03:23 +0800

    update

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FaRegCheckCircle } from "react-icons/fa";
 
 const featuresData = [
